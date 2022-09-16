ca9f720c511e4eccefda7d20dc9434c2c4a348f1
ItsAhmet <74107960+ItsAhmet@users.noreply.github.com>
2022-09-16 16:53:26 -0400

    Create MyDreamSchedule.java

---
+++ MyDreamSchedule.java
@@ -0,0 +1,12 @@
+//Ahmet Yesilbas
+//9/23 Schedule Output (My Dream Schedule)
+//Start Date: 09/21/21
+//End Date: 09/23/21
+//Due Date: 09/24/21
+//Project Description: A print-out of my dream class schedule using print and escape sequences (All in one line of code).
+
+public class MyDreamSchedule {
+	public static void main(String args[]) {
+		System.out.print("Period\tClass\t\tComment\n\n\n1\tBattlefield V\t\"Nice Game (Lots of hackers though).\"\n\n2\tDiscord\t\t\"Check if new people joined your server.\"\n\n3\tComputer Prog.\t\"Java is nice.\"\n\n4\tSpanish\t\t\"Don't forget to submit the homework.\"\n\n5\tLanguage Arts\t\"Boring, lots of homework :/\"\n\n6\tMath\t\t\"Easy class.\"\n\n7\tPE\t\t\"Really exhausting.\"\n\n8\tMinecraft SMP\t\"Game time.\"");
+	}
+}
